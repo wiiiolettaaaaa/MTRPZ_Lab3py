00a8beaf75d6a81a528ce5886ef59bdf135dc905
wiiiolettaaaaa <160237848+wiiiolettaaaaa@users.noreply.github.com>
2025-05-11 17:05:54 +0300

    Changed Dockerfile from optimised to unoptimised. Added 3 extra lines in api.py

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -9,5 +9,8 @@ def hello_world() -> dict:
         'Name': 'Violetta',
         'Last_name': 'Yukhnenko',
         'Age': 18,
-        'Group_code': 'IM-31'
+        'Group_code': 'IM-31',
+        'Year': 2,
+        'Form of study': 'Full-time',
+        'Email': 'yukhnenko.violetta@1ll.kpi.ua'
     }
